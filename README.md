fcb8ddda2b0d1193d00ce293754cb675b517b52a
Johnny Wu <KarasAya@gmail.com>
2017-03-14 14:57:21 +0800

    first workable version

---
+++ README.md
@@ -0,0 +1,19 @@
+## Preview Server
+
+A http server serve static files and directories for quick preview
+
+This programe will simply run a [http-server](https://github.com/indexzero/http-server) and print your ip address and a qrcode in the terminal.
+
+
+
+## Install
+
+```bash
+npm install --save-dev preview-server
+```
+
+## Usage
+
+```bash
+preview --port 8520
+```
---
+++ run.js
@@ -0,0 +1,62 @@
+'ues strict';
+
+const help = `
+  Usage: preview [options] [path]
+
+  Options:
+    -h, --help      Print this usage message.
+    -p, --port      Specify the port.
+    -e, --entry     Specify the entry dir.
+`;
+
+(() => {
+  const argv = process.argv.slice(1);
+  const option = {
+    port: 8001,
+    entry: '',
+    help: false,
+    file: './',
+  };
+
+  for (let i = 0; i < argv.length; ++i) {
+    if (argv[i] === '--port' || argv[i] === '-p') {
+      option.port = argv[++i];
+    } else if (argv[i] === '--entry' || argv[i] === '-e') {
+      option.entry = argv[++i];
+    } else if (argv[i] === '--help' || argv[i] === '-h') {
+      option.help = true;
+      break;
+    } else {
+      option.file = argv[i];
+    }
+  }
+
+  // print help message
+  if (option.help) {
+    console.log(help);
+    return;
+  }
+
+  //
+  const ip = require('ip');
+  const qrcode = require('qrcode-terminal');
+  const { spawn } = require('child_process');
+
+  function blue(str) {
+    return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m';
+  }
+
+  let addr = ip.address();
+  let url = `http://${addr}:${option.port}/${option.entry}`;
+
+  console.log(blue(url));
+  qrcode.generate(url, code => {
+    console.log(code);
+
+    spawn('http-server', [
+      option.file, '-a', 'localhost', '-p', option.port, '-c-1'
+    ], {
+        stdio: 'inherit'
+      });
+  });
+})();
